507c8791b136a3654f4b3d5c9204483562b9ec8d
Corentin Royet <corentin.royet@proton.me>
2025-05-09 14:54:47 +0200

    main code update

--- main_board_code/src/main.cpp
+++ main_board_code/src/main.cpp
@@ -1,6 +1,14 @@
 #include <Arduino.h>
 
-const int pwmPin = 17;
+const int fanPwm = 18;
+const int fanSpeed = 26 ;
+const int fcTemp = 40 ;
+const int fcVoltage = 28 ;
+const int fcCurrrent = 27 ;
+const int evPurge = 3 ;
+const int boardTemp = 10 ;
+const int canRx = 30 :
+const int canTx = 31 ;
 
 void setup() {
   analogWriteFrequency(pwmPin, 25000);  // PWM à 25 kHz
@@ -11,10 +19,10 @@ void setup() {
 void loop() {
   for (int duty = 0; duty <= 255; duty++) {
     analogWrite(pwmPin, duty);
-    delay(10);
+    delay(100);
   }
   for (int duty = 255; duty >= 0; duty--) {
     analogWrite(pwmPin, duty);
-    delay(10);
+    delay(100);
   }
 }
